e3bcfbe844c84bc1c044072f277cce359200a9a2
Ellen Houghton <ellen.a.ash@gmail.com>
2023-09-03 15:01:30 +0100

    working on disclaimer pop up

--- application/static/scripts/script.js
+++ application/static/scripts/script.js
@@ -1,8 +1,3 @@
-// function disclaimer() {
-//     document.getElementById("disclaimer").innerHTML="DISCLAIMER: This website was designed by programming students and while useful, does not offer financial advice.";
-// }
-
 function disclaimer() {
     confirm("DISCLAIMER: This website was designed by a student programmer and while useful, does not offer financial advice.")
-    document.getElementById("disclaimer")
 }
