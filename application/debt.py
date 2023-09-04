c0286eb2a06c894d27a682ef747db600e3f5b2fb
Ellen Houghton <ellen.a.ash@gmail.com>
2023-09-04 20:10:26 +0100

    reworking debt entry and summary

--- application/debt.py
+++ application/debt.py
@@ -1,91 +1,9 @@
 class Debt:
-    def __init__(self, amount, interest, repayment):
+    def __init__(self, identifier, amount, interest, repayment):
+        self.identifier = identifier
         self._amount = amount
         self._interest = interest
         self._repayment = repayment
 
-        if self._amount:
-            self._amount = int(self._amount)
-        else:
-            self._amount = 0
 
-        if self._interest:
-            self._interest = int(self._interest)
-        else:
-            self._interest = 5
-        
-        if self._repayment:
-            self._repayment = int(self._repayment)
-        else:
-            self._repayment = 10
-            # entering a repayment of zero will break our debt calculators 
-            # (there is a while loop that continues until a loan amount is paid - reaches a zero value, if repayment = 0 it creates an infinite loop) 
-            # so we have a default repayment of 10
-        
-    def get_amount(self):
-        return self._amount
-      
-    def get_interest(self):
-        return self._interest
-       
-    def get_repayment(self):
-        return self._repayment
-    
-    def get_stack_months(self):
-        return self._stack_months
-    
-    def get_stack_years(self):
-        return self._stack_years
-    
-    def get_snowball_months(self):
-        return self._snowball_months
-    
-    def get_snowball_years(self):
-        return self._snowball_years
-    
-    def get_avalanche_months(self):
-        return self._avalanche_months
-    
-    def get_avalanche_years(self):
-        return self._avalanche_years
-    
-    def get_debt_list(self):
-        return [self._amount, self._interest, self._repayment]
-
-    def set_stack_months(self, months):
-        self._stack_months = months
-        self._stack_years = int(months/12)
-    
-    def set_snowball_months(self, months):
-        self._snowball_months = months
-        self._snowball_years = int(months/12)
-    
-    def set_avalanche_months(self, months):
-        self._avalanche_months = months
-        self._avalanche_years = int(months/12)
-    
-    def set_comparison_type_months(self, num_of_months, comparison_type):
-        if comparison_type == 'stack':
-            self.set_stack_months(num_of_months)
-        elif comparison_type == 'snowball':
-            self.set_snowball_months(num_of_months)
-        elif comparison_type == 'avalanche':
-            self.set_avalanche_months(num_of_months)
-        else:
-            raise "I don't know the comparison_type"
-        # if all the conditions fail then raise an exception
-    
-    def comparison_dict(self):
-        return {
-            'amount': self._amount,
-            'interest': self._interest,
-            'repayment': self._repayment,
-            'stack_paid_months': self._stack_months,
-            'stack_paid_years': self._stack_years,
-            'snowball_paid_months': self._snowball_months,
-            'snowball_paid_years': self._snowball_years,
-            'avalanche_paid_months': self._avalanche_months,
-            'avalanche_paid_years': self._avalanche_years
-        }
-    
     
--- application/routes.py
+++ application/routes.py
@@ -7,6 +7,10 @@ from application.debt_entry_form import DebtEntryForm
 
 bundle = Bundle()
 
+#Global Variables
+counter = 0
+debt_list = []
+
 @app.route('/')
 def index():
     return render_template('index.html')
@@ -25,12 +29,55 @@ def debt_entry():
         if not identifier or not amount or not min_repayment or not interest_rate:
             error = 'Please enter a debt name, amount, interest rate and minimum repayment, calculations and results set are based on the data supplied'
         
+        if amount <= 0 or min_repayment <= 0 or interest_rate <= 0:
+            error = 'Debt must be greater than £0'
+
+        if min_repayment <= 0:
+            error = 'Minimum Repayment must be greater than £0'
+        
         else:
-            # add the debts to a debt object and create a record
-            # maybe need a count variable to assign identifiers to the debts
-            # need to carry a variable for the sample data
-            return render_template('debt_summary.html')
+            counter = counter +1
+            if counter == 1:
+                debt1 = Debt(identifier, amount, interest_rate, min_repayment)
+                debt_list.append(debt1)
+                return render_template('debt_summary.html', debt_list=debt_list)
+            elif counter == 2:
+                debt2 = Debt(identifier, amount, interest_rate, min_repayment)
+                debt_list.append(debt2)
+                return render_template('debt_summary.html', debt_list=debt_list)
+            elif counter == 3:
+                debt3 = Debt(identifier, amount, interest_rate, min_repayment)
+                debt_list.append(debt3)
+                return render_template('debt_summary.html', debt_list=debt_list)
+            else:
+                error = "Enter upto 3 debts"
+                return render_template('debt_summary.html', debt_list=debt_list)
+            
     return render_template('debt_form.html', form=form, message=error)
+
+@app.route('/debt_summary')
+def debt_summary():
+            
+            # UNPARCEL THE DEBTS, WIZARD THEM AND SEND BACK THE DATA
+
+            # send the debt objects into the comparison calc to find out how long each debt takes to pay off using each debt repayment approach (stack, snowball, avalanche)
+            bundle.debt_comparison_calc(debt_object1, debt_object2, debt_object3)
+
+            #sort the objects ready for display on the page
+            nested_list = [debt_object1, debt_object2, debt_object3]
+            order_list = [1, 2, 3]
+
+            stack_approach = sorted(nested_list, key=lambda debt_object: debt_object.get_interest(), reverse=True) # sort the debt objects, largest interest rate first
+            stack_dict = {order:debt_object for order, debt_object in zip(order_list, stack_approach)}
+
+            snowball_approach = sorted(nested_list, key=lambda debt_object: debt_object.get_amount())  # sorted by loan size ascending
+            snowball_dict = {order:debt_object for order, debt_object in zip(order_list, snowball_approach)}
+
+            avalanche_approach = sorted(nested_list, key=lambda debt_object: debt_object.get_amount(), reverse=True)  # sorted by loan size descending
+            avalanche_dict = {order:debt_object for order, debt_object in zip(order_list, avalanche_approach)}
+            # uses a dictionary to hold the order to avoid an object refernce problem
+            # In Python 3.6, the built-in dict class now keeps its items ordered as well
+
 
 @app.route('/debt_comparison_form', methods=['GET', 'POST'])
 def debt_comparison():
