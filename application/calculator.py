b70318319513f55980ceadd7a690af836ae3ea84
Ellen Houghton <ellen.a.ash@gmail.com>
2023-09-05 20:02:54 +0100

    I have created an infinite loop problem

--- application/calculator.py
+++ application/calculator.py
@@ -41,32 +41,50 @@ class Calculator:
 	
 	def comparison_calc(self, repayment_method):
 		# rewrite this so that you pay each debt, every month and put the left overs towards the stack etc
-		num_of_months = 0
-		extra_repayment = 0
-		left_over = 0
-		for debt in list_of_debts:
-			balance = debt.get_amount()
-			repayment = debt.get_repayment()
-			try:
-				while balance > 0:
-					if balance < repayment:
-						left_over = repayment - balance
-						balance = 0
-						balance = balance - repayment - extra_repayment - left_over
-						left_over = 0
-						num_of_months += 1
-			except Exception as ex:
-				raise ValueError("False argument in comparison_calc")
-			debt.set_comparison_type_months(num_of_months, comparison_type)
-            extra_repayment += repayment
+		if repayment_method == 'stack_approach':
+			self.stack_sorter()
+		elif repayment_method == 'snowball_approach':
+			self.snowball_sorter()
+		else:
+			self.avalanche_sorter()
+
+		for debt in self.debt_list:
+			# add a balance to deplete
+			debt['balance'] = debt['amount']
+			# add a key to store the number of months to repay using each method
+			debt[repayment_method] = 0
+		
+		# calculate the extra money put towards debt repayment
+		extra_repayment = self.total_repayment-self.total_min_repayment
+
+		# cycle through the debts until they are paid and handle the extra repayments
+		# creates an infinite loop at the moment
+		while self.debt_list[-1]['balance'] > 0:
+			repayment = self.total_repayment
+			for debt in self.debt_list:
+				print(debt['balance'])
+				if debt['balance'] > debt['repayment']:
+					debt['balance'] -= debt['repayment'] # pay off the minimum balance
+					repayment -= debt['repayment'] # reduce the monthly repayment by paid amount
+					debt[repayment_method] += 1 # add 1 to the count of months
+					debt['balance'] -= repayment
+					repayment = 0 # handle the extra money
+				else:
+					final_payment = debt['balance']
+					debt['balance'] - final_payment
+					repayment - final_payment
+				print(debt['balance'])
 
 if __name__ == '__main__':
 	MrsTester = Calculator()
 	MrsTester.add_debt_to_list({'identifier': 'test debt 1', 'amount': 1000, 'interest': 2, 'repayment': 100})
 	MrsTester.add_debt_to_list({'identifier': 'test debt 2', 'amount': 5000, 'interest': 5, 'repayment': 250})
+
+	# test sorter
 	MrsTester.stack_sorter()
 	MrsTester.snowball_sorter()
 	MrsTester.avalanche_sorter()
-	
 
-        # self.comparison_calc(avalanche, 'avalanche')
+	# test comparisoncalc
+	MrsTester.comparison_calc('stack_approach')
+
