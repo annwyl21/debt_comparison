786ff47ba14d71a59d64358770fcbc41fbf615bb
Ellen Houghton <ellen.a.ash@gmail.com>
2023-09-06 15:05:58 +0100

    infinite loop resolved, new bug in while loop found

--- application/calculator.py
+++ application/calculator.py
@@ -18,62 +18,67 @@ class Calculator:
 		return self.total_min_repayment
 	
 	def set_total_repayment(self, repayment_commitment):
-		self.total_repayment = repayment_commitment
-
+		if repayment_commitment > self.total_min_repayment:
+			self.total_repayment = repayment_commitment
+		else:
+			self.total_repayment = self.total_min_repayment
 	
 	def stack_sorter(self):
 		# organises the debts by interest rate (largest first)
 		stack = self.debt_list
 		debt_stack = sorted(stack, key=lambda debt: debt['interest'], reverse=True)
-		print(debt_stack)
+		return debt_stack
 
 	def snowball_sorter(self):
 		# organises the debts by loan size (smallest first)
 		snowball = self.debt_list
 		debt_snowball = sorted(snowball, key=lambda debt: debt['amount'])
-		print(debt_snowball)
+		return debt_snowball
 	
 	def avalanche_sorter(self):
 		# organises the debts by loan size (largest first)
 		avalanche = self.debt_list
 		debt_avalanche = sorted(avalanche, key=lambda debt: debt['amount'], reverse=True)
-		print(debt_avalanche)
+		return debt_avalanche
 	
 	def comparison_calc(self, repayment_method):
 		# rewrite this so that you pay each debt, every month and put the left overs towards the stack etc
 		if repayment_method == 'stack_approach':
-			self.stack_sorter()
+			my_debt_list = self.stack_sorter()
 		elif repayment_method == 'snowball_approach':
-			self.snowball_sorter()
+			my_debt_list = self.snowball_sorter()
 		else:
-			self.avalanche_sorter()
+			my_debt_list = self.avalanche_sorter()
 
-		for debt in self.debt_list:
+		for debt in my_debt_list:
 			# add a balance to deplete
 			debt['balance'] = debt['amount']
 			# add a key to store the number of months to repay using each method
 			debt[repayment_method] = 0
 		
-		# calculate the extra money put towards debt repayment
-		extra_repayment = self.total_repayment-self.total_min_repayment
-
 		# cycle through the debts until they are paid and handle the extra repayments
-		# creates an infinite loop at the moment
-		while self.debt_list[-1]['balance'] > 0:
+		# find the last debt in the list
+		last_debt = my_debt_list[-1] 
+		while last_debt['balance'] > 0: # BUG while any of the debt balances are above 0 keep paying them, not sure how to write that as while loop
 			repayment = self.total_repayment
-			for debt in self.debt_list:
-				print(debt['balance'])
+			for debt in my_debt_list:
 				if debt['balance'] > debt['repayment']:
-					debt['balance'] -= debt['repayment'] # pay off the minimum balance
-					repayment -= debt['repayment'] # reduce the monthly repayment by paid amount
-					debt[repayment_method] += 1 # add 1 to the count of months
-					debt['balance'] -= repayment
-					repayment = 0 # handle the extra money
+					debt['balance'] = debt['balance'] - debt['repayment'] # pay off the minimum balance
+					repayment = repayment - debt['repayment'] # reduce the monthly repayment by paid amount
+					debt[repayment_method] = debt[repayment_method] + 1 # add 1 to the count of months
+					print(debt['identifier'], debt['balance'], debt['stack_approach'])
 				else:
 					final_payment = debt['balance']
-					debt['balance'] - final_payment
+					debt['balance'] = debt['balance'] - final_payment
 					repayment - final_payment
-				print(debt['balance'])
+			# add a 2nd for loop to check the balance and ay the extra money towards each debt in turn
+			for index in range(len(my_debt_list)-1):
+				debt_to_check = my_debt_list[index]
+				if debt_to_check['balance'] > 0:
+					debt['balance'] = debt['balance'] - repayment
+					repayment = 0 # handle the extra money, the pennies left etc
+
+		return my_debt_list
 
 if __name__ == '__main__':
 	MrsTester = Calculator()
@@ -81,10 +86,13 @@ if __name__ == '__main__':
 	MrsTester.add_debt_to_list({'identifier': 'test debt 2', 'amount': 5000, 'interest': 5, 'repayment': 250})
 
 	# test sorter
-	MrsTester.stack_sorter()
-	MrsTester.snowball_sorter()
-	MrsTester.avalanche_sorter()
+	#MrsTester.stack_sorter()
+	#MrsTester.snowball_sorter()
+	#MrsTester.avalanche_sorter()
+
+	MrsTester.set_total_repayment(350) # Mrs Tester decides how much she can afford, not alot it is just the minimum balance required!
 
 	# test comparisoncalc
-	MrsTester.comparison_calc('stack_approach')
-
+	debts_paid = MrsTester.comparison_calc('stack_approach')
+	for debt in debts_paid:
+		print(debt['identifier'], debt['stack_approach'])
