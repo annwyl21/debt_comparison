35e92563abe93395afeea2ca3130bbd0ad37a65c
Ellen Houghton <ellen.a.ash@gmail.com>
2023-09-23 14:58:49 +0100

    fix fieldset legends

--- application/data_visualisation.py
+++ application/data_visualisation.py
@@ -43,22 +43,19 @@ class Graphs:
 				for debt, paid in result.items():
 					debt_name.append(debt)
 					paid_months.append(paid)
-					self.debt_bar(debt_name, paid_months)
-					plt.savefig('application/static/images/debt_bar_stack.png', transparent=True)
+					self.debt_bar_stack(debt_name, paid_months)
 			elif approach == 'snowball':
 				for debt, paid in result.items():
 					debt_name.append(debt)
 					paid_months.append(paid)
-					self.debt_bar(debt_name, paid_months)
-					plt.savefig('application/static/images/debt_bar_snowball.png', transparent=True)
+					self.debt_bar_snowball(debt_name, paid_months)
 			else:
 				for debt, paid in result.items():
 					debt_name.append(debt)
 					paid_months.append(paid)
-					self.debt_bar(debt_name, paid_months)
-					plt.savefig('application/static/images/debt_bar_avalanche.png', transparent=True)
+					self.debt_bar_avalanche(debt_name, paid_months)
 	
-	def debt_bar(self, debt_name, paid):
+	def debt_bar_stack(self, debt_name, paid):
 		plt.figure(figsize=(3,2))
 		cmap = mcolors.LinearSegmentedColormap.from_list('custom_orange', ['#FFDAB9', '#e43e26'])
 		norm = plt.Normalize(min(paid), max(paid))
@@ -80,7 +77,56 @@ class Graphs:
 			color=colour_rgb)  # text color
 		plt.yticks([])
 		plt.xticks(color=colour_rgb)
-		plt.xlabel('Debt paid in full (months)', color=colour_rgb)
+		plt.savefig('application/static/images/debt_bar_stack.png', transparent=True)
+
+	def debt_bar_snowball(self, debt_name, paid):
+		plt.figure(figsize=(3,2))
+		cmap = mcolors.LinearSegmentedColormap.from_list('custom_orange', ['#FFDAB9', '#e43e26'])
+		norm = plt.Normalize(min(paid), max(paid))
+		colors = cmap(norm(paid))
+		bars = plt.barh(debt_name, paid, color=colors)
+		colour_rgb = (69/255, 68/255, 68/255)
+		ax = plt.gca()
+		ax.spines['top'].set_visible(False)
+		ax.spines['right'].set_visible(False)
+		ax.spines['left'].set_color(colour_rgb)
+		ax.spines['bottom'].set_color(colour_rgb)
+		for bar, label in zip(bars, debt_name):
+			width = bar.get_width()
+			plt.text(0.5,
+			bar.get_y() + bar.get_height()/2,  # y position (centered in the bar)
+			label,  # text value
+			va='center',  # vertical alignment
+			ha='left',   # horizontal alignment
+			color=colour_rgb)  # text color
+		plt.yticks([])
+		plt.xticks(color=colour_rgb)
+		plt.savefig('application/static/images/debt_bar_snowball.png', transparent=True)
+
+	def debt_bar_avalanche(self, debt_name, paid):
+		plt.figure(figsize=(3,2))
+		cmap = mcolors.LinearSegmentedColormap.from_list('custom_orange', ['#FFDAB9', '#e43e26'])
+		norm = plt.Normalize(min(paid), max(paid))
+		colors = cmap(norm(paid))
+		bars = plt.barh(debt_name, paid, color=colors)
+		colour_rgb = (69/255, 68/255, 68/255)
+		ax = plt.gca()
+		ax.spines['top'].set_visible(False)
+		ax.spines['right'].set_visible(False)
+		ax.spines['left'].set_color(colour_rgb)
+		ax.spines['bottom'].set_color(colour_rgb)
+		for bar, label in zip(bars, debt_name):
+			width = bar.get_width()
+			plt.text(0.5,
+			bar.get_y() + bar.get_height()/2,  # y position (centered in the bar)
+			label,  # text value
+			va='center',  # vertical alignment
+			ha='left',   # horizontal alignment
+			color=colour_rgb)  # text color
+		plt.yticks([])
+		plt.xticks(color=colour_rgb)
+		plt.savefig('application/static/images/debt_bar_avalanche.png', transparent=True)
+
 		
 	
 
